aa7cce3c4cb15e67f7dc913e648a43c1da74bb34
Sravani <nettikantisravani3@gmail.com>
2025-10-23 18:31:06 +0530

    Exception and Logging is completed

---
+++ networksecurity/logging/logger.py
@@ -0,0 +1,17 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f'{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log'
+
+logs_path=os.path.join(os.getcwd(),'logs')
+os.makedirs(logs_path,exist_ok=True)#ensures that we do not create the logs foilder again if we have it already
+
+lOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+logging.basicConfig(
+    filename=lOG_FILE_PATH,
+    format='[ %(asctime)s] %(lineno)d %(name)s -%(levelname)s -%(message)s',
+    level=logging.INFO
+)
+
